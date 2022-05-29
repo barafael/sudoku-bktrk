15538b2f71acf0075459cef52d80ab06072b137a
Rafael Bachmann <rafael.bachmann.93@gmail.com>
2022-05-30 00:17:54 +0200

    Add insane display impl

--- src/board.rs
+++ src/board.rs
@@ -1,4 +1,5 @@
 use derive_more::{Deref, DerefMut};
+use itertools::Itertools;
 use serde::{Deserialize, Serialize};
 
 use crate::constants::N;
@@ -22,6 +23,7 @@ use std::fmt::Display;
 #[cfg_attr(test, derive(proptest_derive::Arbitrary))]
 pub struct Board(pub [[usize; N]; N]);
 
+/*
 // TODO: make beautiful
 impl Display for Board {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
@@ -35,12 +37,54 @@ impl Display for Board {
         Ok(())
     }
 }
+*/
+
+impl Display for Board {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let hor_line = Some("------------\n".to_string());
+        hor_line
+            .iter()
+            .cloned()
+            .chain(
+                self.into_iter()
+                    .flatten()
+                    .map(|n| format!("{n}"))
+                    .chunks(27)
+                    .into_iter()
+                    .map(|chunk| {
+                        chunk
+                            .chunks(9)
+                            .into_iter()
+                            .map(|line| {
+                                line.into_iter()
+                                    .chunks(3)
+                                    .into_iter()
+                                    .map(|chunk| chunk.chain(Some("|".to_string())))
+                                    .flatten()
+                                    .chain(Some("\n".to_string()))
+                                    .collect_vec()
+                            })
+                            .flatten()
+                            .chain(hor_line.clone())
+                            .collect_vec()
+                    })
+                    .flatten(),
+            )
+            .map(|v| write!(f, "{}", v))
+            .collect::<Result<(), _>>()
+    }
+}
 
 #[cfg(test)]
 mod test {
     use super::*;
-
     use proptest::prelude::*;
+
+    #[test]
+    fn displays() {
+        let board = Board::default();
+        println!("{board}");
+    }
 
     proptest! {
         #[test]
